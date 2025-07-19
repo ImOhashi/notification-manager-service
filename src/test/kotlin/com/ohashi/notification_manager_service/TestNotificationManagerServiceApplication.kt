ea9ffc85a8a4a9774198bc6f1fb232d08bee34f6
ImOhashi <leonardoohashi.mkt@gmail.com>
2025-07-18 22:42:21 -0300

    fix: remove unused tests configuration and initial boilerplate

--- src/test/kotlin/com/ohashi/notification_manager_service/TestNotificationManagerServiceApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.ohashi.notification_manager_service
-
-import org.springframework.boot.fromApplication
-import org.springframework.boot.with
-
-
-fun main(args: Array<String>) {
-	fromApplication<NotificationManagerServiceApplication>().with(TestcontainersConfiguration::class).run(*args)
-}
