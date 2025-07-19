37ff1a7fdc15c2120c8f9a738ae18caf16cbb821
ImOhashi <leonardoohashi.mkt@gmail.com>
2025-07-18 23:18:44 -0300

    chore: add kafka config

---
+++ src/main/kotlin/com/ohashi/notification_manager_service/resources/kafka/config/KafkaAdminConfig.kt
@@ -0,0 +1,31 @@
+package com.ohashi.notification_manager_service.resources.kafka.config
+
+import org.apache.kafka.clients.admin.AdminClientConfig
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.TopicBuilder
+import org.springframework.kafka.core.KafkaAdmin
+import org.springframework.kafka.core.KafkaAdmin.NewTopics
+
+@Configuration
+class KafkaAdminConfig(
+    @Value("\${app.kafka.topics.email-topic.name}") private val emailTopic: String,
+    private val properties: KafkaProperties
+) {
+
+    @Bean
+    fun kafkaAdmin(): KafkaAdmin {
+        val configs: Map<String, Any> = mapOf(
+            AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG to properties.bootstrapServers
+        )
+
+        return KafkaAdmin(configs)
+    }
+
+    @Bean
+    fun newTopics(): NewTopics = KafkaAdmin.NewTopics(
+        TopicBuilder.name(this.emailTopic).partitions(1).build()
+    )
+}
