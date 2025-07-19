385ec44023f4c941da079c88d1ead4c11be6af81
ImOhashi <leonardoohashi.mkt@gmail.com>
2025-07-19 01:16:57 -0300

    fix: fix kafka config, remove unused create topic method

--- src/main/kotlin/com/ohashi/notification_manager_service/resources/kafka/config/KafkaAdminConfig.kt
+++ src/main/kotlin/com/ohashi/notification_manager_service/resources/kafka/config/KafkaAdminConfig.kt
@@ -1,18 +1,13 @@
 package com.ohashi.notification_manager_service.resources.kafka.config
 
 import org.apache.kafka.clients.admin.AdminClientConfig
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.kafka.config.TopicBuilder
 import org.springframework.kafka.core.KafkaAdmin
-import org.springframework.kafka.core.KafkaAdmin.NewTopics
 
 @Configuration
 class KafkaAdminConfig(
-    @Value("\${app.kafka.topics.email-topic.name}") private val emailTopicName: String,
-    @Value("\${app.kafka.topics.email-topic.partitions}") private val emailTopicPartitions: Int,
     private val properties: KafkaProperties
 ) {
 
---
+++ src/main/kotlin/com/ohashi/notification_manager_service/resources/kafka/config/KafkaProducerConfig.kt
@@ -0,0 +1,33 @@
+package com.ohashi.notification_manager_service.resources.kafka.config
+
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+import org.springframework.kafka.support.serializer.JsonSerializer
+
+@Configuration
+class KafkaProducerConfig(
+    private val properties: KafkaProperties
+) {
+
+    @Bean
+    fun producerFactory(): ProducerFactory<String, String> {
+        val configProps: Map<String, Any> = mapOf(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to properties.bootstrapServers,
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class,
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to JsonSerializer::class
+        )
+
+        return DefaultKafkaProducerFactory(configProps)
+    }
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, String> {
+        return KafkaTemplate(producerFactory())
+    }
+}
